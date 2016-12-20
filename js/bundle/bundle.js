408bee7f9da94737581babe586e6c5f6da1debc3
mspiew <m.a.spiewack@me.com>
2016-12-20 18:09:53 -0500

    Add test skybox

--- js/bundle/bundle.js
+++ js/bundle/bundle.js
@@ -156,6 +156,25 @@
 	
 	
 	
+	
+	var imagePrefix = "./../textures/skybox/";
+	var directions  = ["posx", "negx", "posy", "negy", "posz", "negz"];
+	var imageSuffix = ".png";
+	var skyGeometry = new THREE.CubeGeometry( 5000, 5000, 5000 );
+	
+	var materialArray = [];
+	for (var i = 0; i < 6; i++)
+	  materialArray.push( new THREE.MeshBasicMaterial({
+	    map: THREE.ImageUtils.loadTexture( imagePrefix + directions[i] + imageSuffix ),
+	    side: THREE.BackSide
+	  }));
+	var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+	var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+	scene.add( skyBox );
+	
+	
+	
+	
 	controls = new THREE.OrbitControls(camera, renderer.domElement);
 	controls.target = earth.obj.position;
 	camera.position = earth.position;
